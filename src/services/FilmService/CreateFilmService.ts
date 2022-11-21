97b25dc9d5fbf22d5c8fb35e52029315385e58a9
Thiago Gasparini <thiago_engprod@outlook.com>
2022-11-21 00:57:36 -0300

    add correções

--- src/services/FilmService/CreateFilmService.ts
+++ src/services/FilmService/CreateFilmService.ts
@@ -1,6 +1,5 @@
 import { Category } from "@modules/categories/typeorm/entities/Category";
 import { Film } from "@modules/categories/typeorm/entities/Film";
-import RedisCache from '@shared/cache/RedisCache';
 import AppError from "@shared/errors/AppError";
 import { getRepository } from "typeorm";
 
@@ -20,11 +19,7 @@ export class CreateFilmService {
       throw new AppError('Category does not exists');
     }
 
-    const redisCache = new RedisCache();
-
     const film = repo.create({ name, description, category_id, durations });
-
-    await redisCache.invalidate('api-films-FILM_LIST');
 
     await repo.save(film);
 
--- src/services/FilmService/GetAllFilmsService.ts
+++ src/services/FilmService/GetAllFilmsService.ts
@@ -1,22 +1,13 @@
 import { Film } from '@modules/categories/typeorm/entities/Film';
 import { getRepository } from 'typeorm';
-import RedisCache from '@shared/cache/RedisCache';
 
 export class GetAllFilmsService {
   async execute() {
     const repo = getRepository(Film);
-    const redisCache = new RedisCache();
 
-    let films = await redisCache.recover<Film[]>(
-      'api-films-FILM_LIST'
-    );
-
-    if (!films) {
-      films = await repo.find({
-        relations: ['category']
-      }); 
-      await redisCache.save('api-films-FILM_LIST', films);
-    }
+    const films = await repo.find({
+      relations: ['category']
+    });
 
     return films;
   }
