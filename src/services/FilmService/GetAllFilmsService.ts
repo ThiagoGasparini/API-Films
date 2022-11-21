3d04d55e535cc1883174245d64e21c4c41c4fc43
Thiago Gasparini <thiago_engprod@outlook.com>
2022-11-21 09:56:48 -0300

    add correções no redis

--- src/services/FilmService/GetAllFilmsService.ts
+++ src/services/FilmService/GetAllFilmsService.ts
@@ -1,14 +1,22 @@
 import { Film } from '@modules/categories/typeorm/entities/Film';
 import { getRepository } from 'typeorm';
+import RedisCache from '@shared/cache/RedisCache';
 
 export class GetAllFilmsService {
   async execute() {
     const repo = getRepository(Film);
+    const redisCache = new RedisCache();
 
-    const films = await repo.find({
+    let film = await redisCache.recover<Film[]>(
+      'api-films-FILM_LIST'
+    );
+
+    film = await repo.find({
       relations: ['category']
     });
 
-    return films;
+    await redisCache.save('api-films-FILM_LIST', film);
+
+    return film;
   }
 }
